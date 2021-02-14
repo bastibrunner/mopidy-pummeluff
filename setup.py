50ac1dc3de7cf99c005ec0db843c860aaf8b008a
bastibrunner <bastibrunner@users.noreply.github.com>
2021-02-14 11:19:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env python
-'''
-Setup script for Mopidy-Pummeluff module.
-'''
+from setuptools import setup
 
-
-
-from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-    requirements = f.read().strip().split('\n')
-
-with open('requirements_dev.txt') as f:
-    requirements_dev = f.read().strip().split('\n')
-
-setup(
-    name='Mopidy-Pummeluff',
-    use_scm_version=True,
-    url='https://github.com/confirm/mopidy-pummeluff',
-    license='MIT',
-    author='confirm IT solutions',
-    description='Pummeluff is a Mopidy extension which allows you to control Mopidy via RFID tags',
-    long_description=open('README.rst').read()
-)
+setup()
